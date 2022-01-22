bba54a1adceb6b6d8085ca772de580f36a502613
Krumuvecis <66279792+Krumuvecis@users.noreply.github.com>
2022-01-22 19:09:50 +0200

    test stub

---
+++ src/test/java/Test1.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class Test1 {
+    @BeforeAll
+    static void initialize() {
+
+    }
+
+    @Test
+    void test() {
+
+    }
+}
