78456fc4b8d136f870cd4ef90f4867da0875e1cf
Krumuvecis <66279792+Krumuvecis@users.noreply.github.com>
2022-01-24 20:32:19 +0200

    UTF-8 support

--- src/main/java/fileHandler/EncodingUtilities.java
+++ src/main/java/fileHandler/EncodingUtilities.java
@@ -1,11 +1,13 @@
 package fileHandler;
 
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public class EncodingUtilities {
     private static final EncodingType DEFAULT_ENCODING = EncodingType.E_1257;
 
     public enum EncodingType {
+        UTF_8,
         E_1257
         //add new encoding types here
     }
@@ -13,6 +15,9 @@ public class EncodingUtilities {
     protected static Charset encodingTypeToCharset(EncodingType encoding) {
         switch (encoding) {
             //check for other encoding types here
+            case UTF_8 -> {
+                return StandardCharsets.UTF_8;
+            }
             case E_1257 -> {
                 return Charset.forName("Cp1257");
             }
