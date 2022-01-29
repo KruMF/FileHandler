13252956554903010cef2f1e05416190fc5a95ba
Krumuvecis <66279792+Krumuvecis@users.noreply.github.com>
2022-01-29 07:09:01 +0200

    data type test stub

---
+++ src/test/java/DataTypeTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+//TODO: finish this and add javadoc
+public class DataTypeTest {
+
+    @BeforeAll
+    static void initialize() {
+
+    }
+
+    @Test
+    void testPrimitives() {
+        //TODO: finish this
+        assertEquals(true, false, "primitive dataType test not finished yet");
+    }
+}
