56486be1a8b4277e1d4c9922c48bc09c4d91566c
Krumuvecis <66279792+Krumuvecis@users.noreply.github.com>
2022-02-13 12:02:49 +0200

    old code removal

--- src/main/java/fileHandler/dataTypes/IEListETC.java
+++ src/main/java/fileHandler/dataTypes/IEListETC.java
@@ -42,27 +42,4 @@ public class IEListETC {
 //        }
 //        return exportable;
 //    }
-
-    // for colors in future
-    /*
-    if (valueArray.length == 3) {
-        setValue(new Color(
-                Integer.parseInt(valueArray[0]),
-                Integer.parseInt(valueArray[1]),
-                Integer.parseInt(valueArray[2])));
-
-    } else if (valueArray.length == 4) {
-        setValue(new Color(
-                Integer.parseInt(valueArray[0]),
-                Integer.parseInt(valueArray[1]),
-                Integer.parseInt(valueArray[2]),
-                Integer.parseInt(valueArray[3])));
-    }
-
-    return new String[] {
-                    String.valueOf(color.getRed()),
-                    String.valueOf(color.getGreen()),
-                    String.valueOf(color.getBlue()),
-                    String.valueOf(color.getAlpha())};
-    */
 }
